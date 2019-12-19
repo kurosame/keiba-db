4438ce28807a00e5cbf0ecb202cd0ee5ace66d9b
kurosame <kurosame1227@gmail.com>
2019-12-19 17:00:39 +0900

    fix: get race html texts by race links

--- src/default/cmd/main.go
+++ src/default/cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const baseURL = "https://db.netkeiba.com"
-const term = 3
+const term = 1
 
 var now time.Time
 
@@ -42,50 +42,64 @@ func getRaceLinks(eventDateLinks []string) []string {
 	return links
 }
 
+func getRaceTexts(raceLinks []string) [][]string {
+	var texts [][]string
+	for _, l := range raceLinks {
+		time.Sleep(1 * time.Second)
+
+		doc := service.GetHTMLDoc(baseURL + l)
+		texts = append(texts, service.GetTexts(doc, ".race_table_01 > tbody > tr"))
+	}
+
+	return texts
+}
+
 func main() {
 	now = time.Now()
 
-	// eventDateLinks := getEventDateLinks()
-	// raceLinks := getRaceLinks(eventDateLinks)
+	eventDateLinks := getEventDateLinks()
+	raceLinks := getRaceLinks(eventDateLinks)
+	raceTexts := getRaceTexts(raceLinks)
 
-	doc := service.GetHTMLDoc("https://db.netkeiba.com/race/201910021212/")
-	texts := service.GetTexts(doc, ".race_table_01 > tbody > tr")
+	var rows []string
+	for _, texts := range raceTexts {
+		var raceResults []*model.RaceResult
+		for i, t := range texts {
+			// Exclude header
+			if i == 0 {
+				continue
+			}
 
-	var raceResults []*model.RaceResult
-	for i, t := range texts {
-		// Exclude header
-		if i == 0 {
-			continue
+			tr := strings.NewReplacer("</td>\n", "</td>\n", "\n", "").Replace(t)
+			ts := service.SanitizeHTML(tr)
+
+			raceResult := &model.RaceResult{
+				Rank:         ts[0],
+				FrameNumber:  ts[1],
+				HorseNumber:  ts[2],
+				HorseName:    ts[3],
+				Age:          ts[4],
+				Weight:       ts[5],
+				JockeyName:   ts[6],
+				Time:         ts[7],
+				Gap:          ts[8],
+				TimeIndex:    ts[9],
+				Passing:      ts[10],
+				Rise:         ts[11],
+				Odds:         ts[12],
+				Popular:      ts[13],
+				HorseWeight:  ts[14],
+				TrainingTime: ts[15],
+				Comment:      ts[16],
+				Remarks:      ts[17],
+				TrainerName:  ts[18],
+				OwnerName:    ts[19],
+				Prize:        ts[20]}
+			raceResults = append(raceResults, raceResult)
 		}
-
-		tr := strings.NewReplacer("</td>\n", "</td>\n", "\n", "").Replace(t)
-		ts := service.SanitizeHTML(tr)
-
-		raceResult := &model.RaceResult{
-			Rank:         ts[0],
-			FrameNumber:  ts[1],
-			HorseNumber:  ts[2],
-			HorseName:    ts[3],
-			Age:          ts[4],
-			Weight:       ts[5],
-			JockeyName:   ts[6],
-			Time:         ts[7],
-			Gap:          ts[8],
-			TimeIndex:    ts[9],
-			Passing:      ts[10],
-			Rise:         ts[11],
-			Odds:         ts[12],
-			Popular:      ts[13],
-			HorseWeight:  ts[14],
-			TrainingTime: ts[15],
-			Comment:      ts[16],
-			Remarks:      ts[17],
-			TrainerName:  ts[18],
-			OwnerName:    ts[19],
-			Prize:        ts[20]}
-		raceResults = append(raceResults, raceResult)
+		race := &model.Race{RaceResults: raceResults}
+		rows = append(rows, race.ToJSON())
 	}
-	race := &model.Race{RaceResults: raceResults}
 
-	service.OutputJSONL([]string{race.ToJSON()})
+	service.OutputJSONL(rows)
 }
